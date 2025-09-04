6d94c115f07bcb40ba2915f51000682e2984f4e8
Merakiscrap <ch.deantoni@gmail.com>
2025-09-04 12:18:43 +0300

    Update load-components.js

--- js/load-components.js
+++ js/load-components.js
@@ -1,1 +1,25 @@
+// Charger le menu et le footer
+document.addEventListener("DOMContentLoaded", function() {
+  // Charger le menu
+  fetch('components/menu.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('menu-container').innerHTML = data;
+
+      // Ajouter la logique active link
+      const currentPage = window.location.pathname.split('/').pop().replace('.html', '');
+      document.querySelectorAll('.menu-link').forEach(link => {
+        if (link.dataset.page === currentPage || (currentPage === '' && link.dataset.page === 'index')) {
+          link.classList.add('active');
+        }
+      });
+    });
+
+  // Charger le footer
+  fetch('components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer-container').innerHTML = data;
+    });
+});
 
